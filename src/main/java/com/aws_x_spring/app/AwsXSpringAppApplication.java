95749d891bb7ff1547b0cc8c5ebe0db3fb21b1a4
Shubham Mishra <shubhamrmishra999@gmail.com>
2026-09-13 11:28:42 +0530

    first commit

---
+++ src/main/java/com/aws_x_spring/app/AwsXSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.aws_x_spring.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsXSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsXSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws_x_spring/app/AwsXSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws_x_spring.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsXSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
